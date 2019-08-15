df8956888fe9c62cc1953399339f72d83523598a
“weimingjue” <873456775@qq.com>
2019-08-15 10:33:56 +0800

    删除多余无用的内容

--- app/src/main/java/com/wang/hongbaotest/MainActivity.java
+++ app/src/main/java/com/wang/hongbaotest/MainActivity.java
@@ -4,14 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView tv = new TextView(this);
+        tv.setText("开启辅助，打开wx聊天界可看到提示，如没有提示请一键清理所有app或重启手机再试");
+        setContentView(tv);
     }
 
     @Override
