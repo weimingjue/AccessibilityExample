5d699da0346a1047508493f09ede868ef6939a5a
“weimingjue” <873456775@qq.com>
2019-08-15 10:13:56 +0800

    增加打开wx聊天界面提示语

--- app/src/main/java/com/wang/hongbaotest/HongBaoService.java
+++ app/src/main/java/com/wang/hongbaotest/HongBaoService.java
@@ -2,6 +2,10 @@ package com.wang.hongbaotest;
 
 import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HongBaoService extends AccessibilityService {
     private final String TAG = getClass().getName();
@@ -19,6 +23,10 @@ public class HongBaoService extends AccessibilityService {
     //实现辅助功能
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        List<AccessibilityNodeInfo> listBiaoQing = findViewByContentDescription("表情");
+        if (!Utils.isEmptyArray(listBiaoQing)) {
+            Utils.toast("找到wx的表情图标");
+        }
     }
 
     @Override
@@ -36,5 +44,35 @@ public class HongBaoService extends AccessibilityService {
      */
     public static boolean isStart() {
         return mService != null;
+    }
+
+    /**
+     * 此方法效率相对较低,建议使用之后保存id然后根据id进行查找
+     */
+    public List<AccessibilityNodeInfo> findViewByContentDescription(String contentDescription) {
+        ArrayList<AccessibilityNodeInfo> list = new ArrayList<>();
+        AccessibilityNodeInfo rootInfo = getRootInActiveWindow();
+        if (rootInfo == null) return list;
+        findViewByContentDescription(list, rootInfo, contentDescription);
+        rootInfo.recycle();
+        return list;
+    }
+
+    /**
+     * 此方法效率相对较低,建议使用之后保存id然后根据id进行查找
+     */
+    public static void findViewByContentDescription(List<AccessibilityNodeInfo> list, AccessibilityNodeInfo parent, String contentDescription) {
+        if (parent == null) return;
+        for (int i = 0; i < parent.getChildCount(); i++) {
+            AccessibilityNodeInfo child = parent.getChild(i);
+            if (child == null) continue;
+            CharSequence cd = child.getContentDescription();
+            if (cd != null && contentDescription.equals(cd.toString())) {
+                list.add(child);
+            } else {
+                findViewByContentDescription(list, child, contentDescription);
+                child.recycle();
+            }
+        }
     }
 }
--- app/src/main/java/com/wang/hongbaotest/Utils.java
+++ app/src/main/java/com/wang/hongbaotest/Utils.java
@@ -2,9 +2,20 @@ package com.wang.hongbaotest;
 
 import android.widget.Toast;
 
+import java.util.Collection;
+
 public class Utils {
 
     public static void toast(CharSequence cs) {
         Toast.makeText(MyApp.mApp, cs, Toast.LENGTH_SHORT).show();
+    }
+
+    //集合是否是空的
+    public static boolean isEmptyArray(Collection list) {
+        return list == null || list.size() == 0;
+    }
+
+    public static <T> boolean isEmptyArray(T[] list) {
+        return list == null || list.length == 0;
     }
 }
